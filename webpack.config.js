3c214412194e7a7de8d1e7bd127c564415d2c1fb
Georg <geokav87@mail.ru>
2022-01-19 20:16:53 +0300

    webPack+Babel

--- webpack.config.js
+++ webpack.config.js
@@ -13,5 +13,22 @@ module.exports = {
 
   devtool: "source-map",
 
-  module: {}
+  module: {  
+    rules: [ // правила
+    {
+      test: /\.m?js$/,   // находим js файлы
+      exclude: /(node_modules|bower_components)/,  // какие фалый мы исключаем из выборки 
+      use: { // как и что будем использовать
+        loader: 'babel-loader',  // связывает  babel с webpack, его надо установить: 'npm i --save-dev babel-loader'
+        options: { // опции
+            presets: [['@babel/preset-env', {
+                debug: true, // показывает ошибки с иной
+                corejs: 3, // - это библиотека кт опр. какие полифилы нам нужны, чтобы не устанавливать лишнее
+                useBuiltIns: "usage"   // разрешаем corejs автоматически выбирать и устанавливать нужные полифилы
+            }]]
+        }
+      }
+    }
+    ]
+}
 };
